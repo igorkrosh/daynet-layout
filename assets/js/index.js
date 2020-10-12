2982542608631fe08afcef28e2379735bdbb14c7
Igor Kroshkin <igor.krosh@gmail.com>
2020-10-12 16:35:54 +0300

    Новый курсор

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,37 @@
 $(document).ready(function () {
+    SetCursor();
     SetNavbarMenu();
 })
+
+function SetCursor() {
+    let initCursorHeight = $('.cursor .bg').height();
+    let initCursotWidth = $('.cursor .bg').width();
+
+    let animateOptions = {
+        queue: false,
+        duration: 200
+    }
+    
+    $('body').mousemove(function(event) {
+        $('.cursor').css('top', `${event.pageY}px`);
+        $('.cursor').css('left', `${event.pageX}px`);
+    })
+
+    $('a, button').hover(function() {
+        let itemHeight = $(this).height() * 1.5;
+        $('.cursor .bg').animate({
+            height: itemHeight + 'px',
+            width: itemHeight + 'px'
+        }, animateOptions)
+    });
+
+    $('a, button').mouseleave(function() {
+        $('.cursor .bg').animate({
+            height: initCursorHeight + 'px',
+            width: initCursotWidth + 'px'
+        }, animateOptions)
+    });
+}
 
 function SetNavbarMenu() {
     let animateInClass = 'animate__slideInLeft';
