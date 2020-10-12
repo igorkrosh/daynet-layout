428d964be63b0910c1d4671dbfe1792bdd11d360
Igor Kroshkin <igor.krosh@gmail.com>
2020-10-12 14:59:15 +0300

    Navbar и меню - Десктоп. Добавлены новые шрифты

---
+++ assets/js/index.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    SetNavbarMenu();
+})
+
+function SetNavbarMenu() {
+    let animateInClass = 'animate__slideInLeft';
+    let animateOutClass = 'animate__slideOutLeft'
+
+    $('.btn-menu').on('click', function () {
+        if (!$('.navbar-menu').hasClass('animate__animated'))
+        {
+            $('.navbar-menu').addClass('animate__animated');
+        }
+
+        if ($(this).hasClass('active'))
+        {
+            $(this).removeClass('active');
+            $('.navbar-menu').removeClass(animateInClass);
+            $('.navbar-menu').addClass(animateOutClass);
+        }
+        else
+        {
+            $(this).addClass('active');
+            $('.navbar-menu').addClass(animateInClass);
+            $('.navbar-menu').removeClass(animateOutClass);
+        }        
+    })
+}
