91895f53d697e82edb0b0295312a1ab8d0215b85
Igor Kroshkin <igor.krosh@gmail.com>
2020-10-23 00:22:28 +0300

    Новая страница Контакты - >1440px

---
+++ assets/js/page.js
@@ -0,0 +1,72 @@
+// Скрипты для внутренних страниц
+
+$(document).ready(Core)
+
+function Core()
+{
+    SetNavbarMenu();
+    SetCursor();
+}
+
+function SetCursor() // Устанавливает кастомный курсор на странице
+{
+    let initCursorHeight = $('.cursor .bg').height();
+    let initCursotWidth = $('.cursor .bg').width();
+
+    let animateOptions = {
+        queue: false,
+        duration: 200
+    }
+    
+    $('body').mousemove(function(event) {
+        $('.cursor').css('transform', `translate3d(${event.clientX}px, ${event.clientY}px, 0)`)
+    })
+
+    $("a, button, input").on(
+    {
+        mouseenter: function() 
+        {
+            let itemHeight = $(this).height() * 1.5;
+            $('.cursor .bg').animate({
+                height: itemHeight + 'px',
+                width: itemHeight + 'px'
+            }, animateOptions)
+        },
+        mouseleave: function()
+        {
+            $('.cursor .bg').animate({
+                height: initCursorHeight + 'px',
+                width: initCursotWidth + 'px'
+            }, animateOptions)
+        }
+    });
+}
+
+function SetNavbarMenu() // Установка бокового меню навбара
+{
+    let animateInClass = 'animate__slideInLeft';
+    let animateOutClass = 'animate__slideOutLeft'
+
+    $('.btn-menu').on('click', function () {
+        if (!$('.navbar-menu').hasClass('animate__animated'))
+        {
+            $('.navbar-menu').addClass('animate__animated');
+        }
+
+        $('.direction-popup').removeClass('active');
+        $('.navbar').removeClass('active');
+
+        if ($(this).hasClass('active'))
+        {
+            $(this).removeClass('active');
+            $('.navbar-menu').removeClass(animateInClass);
+            $('.navbar-menu').addClass(animateOutClass);
+        }
+        else
+        {
+            $(this).addClass('active');
+            $('.navbar-menu').addClass(animateInClass);
+            $('.navbar-menu').removeClass(animateOutClass);
+        }        
+    })
+}
